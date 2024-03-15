6c3e8d90bb92c266193a5307f89802d111b13ca8
Allan Nava <allannava95@gmail.com>
2024-03-15 15:32:38 +0100

    solve config

--- env/config.go
+++ env/config.go
@@ -16,4 +16,22 @@ type Configuration struct {
 	DbIdleConn          string `env:"DB_IDLE_CONN"`
 	DbMaxConn           string `env:"DB_MAX_CONN"`
 	DbName              string `env:"DB_NAME"`
-)
+}
+
+
+
+func GetEnvConfig() *Configuration {
+	appEnv := os.Getenv("APP_ENV")
+	if appEnv == "" {
+		return
+	}
+	err := godotenv.Load(fmt.Sprintf("./env/.env.%s", strings.ToLower(appEnv)))
+	if err == nil {
+		return
+	}
+	_ = godotenv.Load()
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+    return &cfg
+}
